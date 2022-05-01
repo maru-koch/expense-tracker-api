1400f74ec5d2f7e49a3f369f813d2a8d4a437f76
maru-koch <nwokochafranklyn@gmail.com>
2022-05-01 23:31:58 +0100

    --no-edit

--- expense_tracker_api/settings.py
+++ expense_tracker_api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ldh(*xha0e5vp(ry5_1omnc&dn0z1ja#9w^cy2et_k9150og-o'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
